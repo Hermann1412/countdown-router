912333183b1d170fed4f6b8e3cb50636737cf7a3
Hermann <u6411076@au.edu>
2026-01-16 15:19:43 +0700

    before try

--- countdown-router/vite.config.js
+++ countdown-router/vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/countdown-router/"
+  base: "/countdown-router/coundown-router/",
 
 });
